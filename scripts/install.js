5faf10f0d68ed836754607b69e792bdf5e9b44f9
Michael Chartrand <mchartrand@version10.ca>
2019-05-01 17:19:18 -0400

    up

--- scripts/install.js
+++ scripts/install.js
@@ -135,7 +135,7 @@ function checkAndDownloadBinary() {
       console.log(sass.getBinaryUrl())
       console.log(binaryPath)
 
-      download('https://github.com/sass/node-sass/releases/download/v'+pkg.version+'/'+downloads[i]+'_binding.node', path.join(sass.getBinaryDir(), downloads[i].replace(/_(?=binding\.node)/, '/')), function(err) {
+      download('https://github.com/sass/node-sass/releases/download/v'+pkg.version+'/'+downloads[i]+'_binding.node', path.join(sass.getBinaryDir(), downloads[i].replace(/_(?=binding\.node)/, '/')+"/binding.node"), function(err) {
         if (err) {
           console.error(err);
           return;
